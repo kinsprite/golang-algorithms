b2e0557628aa2fc94ca13cfef71127db7e824550
kinsprite <qxqs11@126.com>
2019-08-27 22:32:19 +0800

    add BubbleSort(items)

--- sort/bubble_sort.go
+++ sort/bubble_sort.go
@@ -1,8 +1,28 @@
 package sort
 
-type LessComp interface {
+// BubbleSortInterface BubbleSort Interface
+type BubbleSortInterface interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
 }
 
-func BubbleSort() {
+// Less IntSlice's less compare
+func (items *IntSlice) Swap(i, j int) {
+	(*items)[i], (*items)[j] = (*items)[j], (*items)[i]
+}
 
+// BubbleSort BubbleSort(items)
+func BubbleSort(items BubbleSortInterface) BubbleSortInterface {
+	length := items.Len()
+
+	for i := length - 1; i > 0; i-- {
+		for j := 1; j <= i; j++ {
+			if items.Less(j, j-1) {
+				items.Swap(j, j-1)
+			}
+		}
+	}
+
+	return items
 }
--- sort/bubble_sort_test.go
+++ sort/bubble_sort_test.go
@@ -1,16 +1,47 @@
 package sort
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestBubbleSort(t *testing.T) {
+	type args struct {
+		items BubbleSortInterface
+	}
 	tests := []struct {
 		name string
+		args args
+		want BubbleSortInterface
 	}{
-		// TODO: Add test cases.
+		{
+			name: "4,3,2,1,0",
+			args: args{
+				items: &IntSlice{4, 3, 2, 1, 0},
+			},
+			want: &IntSlice{0, 1, 2, 3, 4},
+		},
+		{
+			name: "4,2,1,3,0",
+			args: args{
+				items: &IntSlice{4, 2, 1, 3, 0},
+			},
+			want: &IntSlice{0, 1, 2, 3, 4},
+		},
+		{
+			name: "0,2,1,3,4,5",
+			args: args{
+				items: &IntSlice{0, 2, 1, 3, 4, 5},
+			},
+			want: &IntSlice{0, 1, 2, 3, 4, 5},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			BubbleSort()
+			if got := BubbleSort(tt.args.items); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BubbleSort() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
