31df868ba57e5f945abccdea7a8ae3738d59ad25
kinsprite <qxqs11@126.com>
2019-08-29 15:26:28 +0800

    add QuickSort()

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -1,5 +1,34 @@
 package sort
 
+// QuickSort QuickSort
 func QuickSort(items IntSlice) IntSlice {
+	size := len(items)
+
+	if size < 2 {
+		return items
+	}
+
+	pivot := items[0]
+	less := make(IntSlice, 0, size)
+	greater := make(IntSlice, 0, size)
+
+	for i := 1; i < size; i++ {
+		value := items[i]
+
+		if value <= pivot {
+			less = append(less, value)
+		} else {
+			greater = append(greater, value)
+		}
+	}
+
+	QuickSort(less)
+	QuickSort(greater)
+
+	copy(items, less)
+	lessSize := len(less)
+	items[lessSize] = pivot
+	copy(items[lessSize+1:], greater)
+
 	return items
 }
---
+++ sort/quick_sort_test.go
@@ -0,0 +1,32 @@
+package sort
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	type args struct {
+		items IntSlice
+	}
+	tests := []struct {
+		name string
+		args args
+		want IntSlice
+	}{
+		{
+			name: "1,4,3,2",
+			args: args{
+				items: IntSlice{1, 4, 3, 2},
+			},
+			want: IntSlice{1, 2, 3, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := QuickSort(tt.args.items); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("QuickSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
